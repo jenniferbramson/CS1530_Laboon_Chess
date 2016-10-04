643fadda276e89c273244175b0b7df1b35cd21bd
Jennifer Bramson <jib41@pitt.edu>
2016-10-04 18:00:23 -0400

    refactoring

--- src/main/java/chess/view/BoardGraphics.java
+++ src/main/java/chess/view/BoardGraphics.java
@@ -14,41 +14,45 @@ public class BoardGraphics {
   // Puts all the components together to create the screen
   public BoardGraphics() {
     JFrame frame = new JFrame("Laboon Chess");
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     Container content = frame.getContentPane();   // Get reference to content pane
 
-    JPanel timer = this.addTimer();               // Get the timer panel
-    JPanel board = this.addCheckers();            // Get the square board
-    JPanel buttons = this.addButtons();           // Get the buttons panel
+    // Left side of the board has the timer, chess board, and buttons (load and
+    // save) stacked vertically
+    JPanel timer = this.makeTimer();               // Get the timer panel
+    JPanel board = this.makeCheckers();            // Get the square board
+    JPanel buttons = this.makeButtons();           // Get the buttons panel
 
-    JPanel left = new JPanel();                   // Stack the three panels above
+    JPanel left = new JPanel();                    // Stack the three panels above
     left.setLayout(new BorderLayout());
     left.add(timer, BorderLayout.NORTH);
     left.add(board, BorderLayout.CENTER);
     left.add(buttons, BorderLayout.SOUTH);
 
-
-    JPanel takenBlack = this.addTakenBlack();
-    JPanel takenWhite = this.addTakenWhite();
+    // Right side of the board has the the taken black and white pieces stacked
+    // vertically
+    JPanel takenBlack = this.makeTakenBlack();
+    JPanel takenWhite = this.makeTakenWhite();
 
     JPanel right = new JPanel();
     right.setLayout(new GridLayout(2, 1));
     right.add(takenBlack);
     right.add(takenWhite);
 
-
+    // Entire screen has both the leftand right sides of the board put together
+    // in a horizontal fashion
     content.setLayout(new BorderLayout());
     content.add(left, BorderLayout.CENTER);
     content.add(right, BorderLayout.EAST);
 
     frame.setSize(700, 600);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);                     // Do this last
   }
 
 
-  // Puts JButton array in a panel, then adds the panel to the frame's content
-  // The JButton array are the squares
-  private JPanel addCheckers() {
+  // Makes the checkerboard with a JPanel array and adds JLabels around it to
+  // label the rows 1 to 8 and the columns a to h
+  private JPanel makeCheckers() {
     JPanel panel = new JPanel();
     panel.setLayout(new GridLayout(10, 10));
 
@@ -74,7 +78,7 @@ public class BoardGraphics {
       }
     }
 
-    // Create Labels for a through h for the first and last rows
+    // Create Labels for a through h for the first rows
     panel.add(new JLabel(""));  // Corners are empty
     for (int i = 0; i < 8; i++) {
       JLabel label = new JLabel("" + (char)(97 + i));
@@ -111,7 +115,8 @@ public class BoardGraphics {
   } // end addCheckers
 
 
-  private JPanel addButtons() {
+  // Makes the load and save buttons
+  private JPanel makeButtons() {
     JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
     // Add save button
@@ -128,7 +133,7 @@ public class BoardGraphics {
   }
 
   // Placeholder for timer
-  private JPanel addTimer() {
+  private JPanel makeTimer() {
     JPanel panel = new JPanel();
     JLabel label = new JLabel("Timer");   // Place holder for real timer
     label.setHorizontalAlignment(SwingConstants.CENTER);
@@ -138,7 +143,7 @@ public class BoardGraphics {
   }
 
   // Placeholder for taken white pieces
-  private JPanel addTakenWhite() {
+  private JPanel makeTakenWhite() {
     JPanel panel = new JPanel();
     JLabel label = new JLabel("Taken White Pieces");
     panel.setLayout(new GridLayout(3, 1));
@@ -149,7 +154,7 @@ public class BoardGraphics {
   }
 
   // Placeholder for taken black piece
-  private JPanel addTakenBlack() {
+  private JPanel makeTakenBlack() {
     JPanel panel = new JPanel();
     JLabel label = new JLabel("Taken Black Pieces");
     panel.setLayout(new GridLayout(3, 1));
