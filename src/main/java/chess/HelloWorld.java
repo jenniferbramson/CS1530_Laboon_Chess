d1e050ea5bc8482960ead1d1f06ed7b526a40013
Jennifer Bramson <jib41@pitt.edu>
2016-10-04 21:54:25 -0400

    Split up file into multiple files, added some graphics

--- src/main/java/chess/HelloWorld.java
+++ src/main/java/chess/HelloWorld.java
@@ -19,7 +19,7 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     speak();
-    BoardGraphics chessBoard = new BoardGraphics();
+    ConsoleGraphics chessBoard = new ConsoleGraphics();
   }
 
 }
