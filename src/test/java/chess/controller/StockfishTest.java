1a6169ff9776af745e80e206dac4e5fd4982ef61
Pauline Walsh <paulinecwalsh@gmail.com>
2016-10-16 13:29:59 -0400

    Moved Stockfish files to controller folder

---
+++ src/test/java/chess/controller/StockfishTest.java
@@ -0,0 +1,73 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import chess.Stockfish;
+
+public class StockfishTest {
+
+  @Test
+  public void testEngineStart() {
+    Stockfish sf = new Stockfish();
+    // Get OS type to pass in to startEngine() so that the correct file can be executed
+    String os_name = System.getProperty("os.name");
+    boolean started = sf.startEngine(os_name);
+    if (started) sf.stopEngine();
+    assertTrue(started);
+  }
+
+   @Test
+   public void testEngineStop() {
+     Stockfish sf = new Stockfish();
+     String os_name = System.getProperty("os.name");
+     sf.startEngine(os_name);
+     boolean stopped = sf.stopEngine();
+     assertTrue(stopped);
+   }
+
+  @Test
+  public void testUCIMode(){
+    String os_name = System.getProperty("os.name");
+    Stockfish sf = new Stockfish();
+    sf.startEngine(os_name);
+    // Tell the engine to switch to UCI mode
+    sf.send("uci");
+    String output = sf.getOutput();
+    // The engine responds with "uciok" when it is ready to communicate in uci mode
+    assertTrue(output.contains("uciok"));
+    sf.stopEngine();
+  }
+
+  @Test
+  public void testGetFen() {
+    String os_name = System.getProperty("os.name");
+    Stockfish sf = new Stockfish();
+    sf.startEngine(os_name);
+    // Tell the engine to switch to UCI mode
+    sf.send("uci");
+    sf.send("ucinewgame");
+    // Tell the engine to draw the board
+    sf.send("d");
+    String output = sf.getOutput();
+    String fen = sf.getFen();
+    /* The board was just initialized with the starting postions, so the
+       fen string should match the STARTING_POS fen string */
+    assertTrue(fen.equals(Stockfish.STARTING_POS));
+    sf.stopEngine();
+  }
+
+  @Test
+  public void testMovePiece(){
+    String os_name = System.getProperty("os.name");
+    Stockfish sf = new Stockfish();
+    sf.startEngine(os_name);
+    // Tell the engine to switch to UCI mode
+    sf.send("uci");
+    sf.send("ucinewgame");
+    // Move white pawn from d2 to d4 while the rest of the pieces are in the starting positions
+    sf.movePiece("d2d4", sf.STARTING_POS);
+    String fen = sf.getFen();
+    // Fen should contain string with the 4th white pawn in the middle
+    assertTrue(fen.contains("3P4"));
+    sf.stopEngine();
+  }
+}
