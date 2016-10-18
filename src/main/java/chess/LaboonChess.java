d2ec98198d2e6626997e2c2e9153c49b034c37d3
Jennifer Bramson <jib41@pitt.edu>
2016-10-18 10:14:53 -0400

    Added failing tests

--- src/main/java/chess/LaboonChess.java
+++ src/main/java/chess/LaboonChess.java
@@ -4,6 +4,8 @@ public class LaboonChess {
 
   public static void main(String[] args) {
     StartUpMenu chessBoard = new StartUpMenu();
+    //Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    //System.out.println(temp.getSpace(0, 1));
   }
 
 }
--- src/test/java/chess/model/StorageTest.java
+++ src/test/java/chess/model/StorageTest.java
@@ -11,7 +11,7 @@ public class StorageTest {
 		Storage temp = new Storage();
     assertEquals(temp.getSpace(4,4), "");
   }
-	
+
 	@Test
 	public void storageTestMovePiece() {
 		//testing to see if movePiece workds
@@ -21,7 +21,7 @@ public class StorageTest {
 		assertEquals(temp.getSpace(1,2), "");
 		assertEquals(temp.getSpace(4,4), "p");
 	}
-	
+
 	@Test
 	public void storageTestLoadFen() {
 		//testing to see if constructor works passing in fen string
@@ -31,12 +31,44 @@ public class StorageTest {
 		assertEquals(temp.getSpace(3,3), "p");
 		assertEquals(temp.getSpace(5,5), "N");
 	}
-	
+
 	@Test
 	public void storageTestPrintFen() {
 		String s = "rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 0 0";
 		Storage temp = new Storage(s);
 		assertEquals(temp.getFen(),s);
 	}
+
+  @Test
+  public void illegalMove1() {
+  	Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    assertEquals(temp.getSpace(1, 2), "p");
+		temp.movePiece(1, 2, 5, 5);
+    assertNotEquals(temp.getSpace(5, 5), "p");
+  }
+
+  @Test
+  public void illegalMove2() {
+    Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    assertEquals(temp.getSpace(0, 1), "n");
+    temp.movePiece(0, 1, 5, 4);
+    assertNotEquals(temp.getSpace(5, 4), "n");
+  }
+
+  @Test
+  public void legalMove1() {
+    Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    assertEquals(temp.getSpace(0, 1), "n");
+    temp.movePiece(0, 1, 1, 3);
+    assertEquals(temp.getSpace(1, 3), "n");
+  }
+
+  @Test
+  public void legalMove2() {
+    Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    assertEquals(temp.getSpace(0, 1), "n");
+    temp.movePiece(0, 1, 2, 0);
+    assertEquals(temp.getSpace(2, 0), "n");
+  }
 
 }
