8862397d58cf2e0aa01c508f31f5e003970e1a87
Jennifer Bramson <jib41@pitt.edu>
2016-10-18 17:27:57 -0400

    Added failing white piece move tests

--- src/test/java/chess/model/StorageTest.java
+++ src/test/java/chess/model/StorageTest.java
@@ -38,37 +38,85 @@ public class StorageTest {
 		Storage temp = new Storage(s);
 		assertEquals(temp.getFen(),s);
 	}
-/*
+
   @Test
-  public void illegalMove1() {
+  public void illegalMoveBlack1() {
   	Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
-    assertEquals(temp.getSpace(1, 2), "p");
-		temp.movePiece(1, 2, 5, 5);
-    assertNotEquals(temp.getSpace(5, 5), "p");
+    assertEquals(temp.getSpaceChar(1, 1), 'p');
+    if (temp.checkMove(1, 1, 5, 5)) {
+		  temp.movePiece(1, 1, 5, 5);
+    }
+    assertNotEquals(temp.getSpaceChar(5, 5), 'p');
   }
 
   @Test
-  public void illegalMove2() {
+  public void illegalMoveBlack2() {
     Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
-    assertEquals(temp.getSpace(0, 1), "n");
-    temp.movePiece(0, 1, 5, 4);
-    assertNotEquals(temp.getSpace(5, 4), "n");
-  }
-*/
-  @Test
-  public void legalMove1() {
-    Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
-    assertEquals(temp.getSpace(0, 1), "n");
-    temp.movePiece(0, 1, 1, 3);
-    assertEquals(temp.getSpace(1, 3), "n");
+    assertEquals(temp.getSpaceChar(1, 0), 'n');
+    if (temp.checkMove(0, 1, 4, 5)) {
+      temp.movePiece(0, 1, 4, 5);
+    }
+    assertNotEquals(temp.getSpaceChar(5, 4), 'n');
   }
 
   @Test
-  public void legalMove2() {
+  public void illegalMoveWhite1() {
     Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
-    assertEquals(temp.getSpace(0, 1), "n");
-    temp.movePiece(0, 1, 2, 0);
-    assertEquals(temp.getSpace(2, 0), "n");
+    assertEquals(temp.getSpaceChar(1, 6), 'p');
+    if (temp.checkMove(6, 1, 5, 5)) {
+      temp.movePiece(6, 1, 5, 5);
+    }
+    assertNotEquals(temp.getSpaceChar(5, 5), 'p');
+  }
+
+  @Test
+  public void illegalMoveWhite2() {
+    Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    assertEquals(temp.getSpaceChar(1, 7), 'n');
+    if (temp.checkMove(7, 1, 4, 5)) {
+      temp.movePiece(7, 1, 4, 5);
+    }
+    assertNotEquals(temp.getSpaceChar(5, 4), 'n');
+  }
+
+  @Test
+  public void legalMoveBlack1() {
+    Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    assertEquals(temp.getSpaceChar(1, 0), 'n');
+    if (temp.checkMove(0, 1, 2, 2)) {
+      temp.movePiece(0, 1, 2, 2);
+    }
+    assertEquals(temp.getSpaceChar(2, 2), 'n');
+  }
+
+  @Test
+  public void legalMoveBlack2() {
+    Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    assertEquals(temp.getSpaceChar(1, 0), 'n');
+    if (temp.checkMove(0, 1, 2, 0)) {
+      temp.movePiece(0, 1, 2, 0);
+    }
+    assertEquals(temp.getSpaceChar(0, 2), 'n');
+  }
+
+  @Test
+  public void legalMoveWhite1() {
+    Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    assertEquals(temp.getSpaceChar(1, 7), 'n');
+    if (temp.checkMove(7, 1, 5, 2)) {
+      temp.movePiece(7, 1, 5, 2);
+    }
+    assertEquals(temp.getSpaceChar(2, 5), 'n');
+  }
+
+  @Test
+  public void legalMoveWhite2() {
+    Storage temp = new Storage("rnbqkb1r/ppp2ppp/4pn2/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R b KQkq - 2 4");
+    assertEquals(temp.getSpaceChar(1, 7), 'n');
+    if (temp.checkMove(7, 1, 5, 0)) {
+      temp.movePiece(7, 1, 5, 0);
+    }
+    assertEquals(temp.getSpaceChar(0, 5), 'n');
   }
 
 }
