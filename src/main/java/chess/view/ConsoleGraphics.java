0ec7100e39913ed83ab5c5538be19ca761fc71ea
Pauline Walsh <paulinecwalsh@gmail.com>
2016-10-21 19:08:58 -0400

    Integrating stockfish board model with ConsoleGraphics and BoardPanel

--- src/main/java/chess/view/ConsoleGraphics.java
+++ src/main/java/chess/view/ConsoleGraphics.java
@@ -9,16 +9,21 @@ public class ConsoleGraphics {
   protected JButton whiteTurn;
   protected JButton blackTurn;
   protected static JFrame frame;
-  
+  private static final Stockfish stockfish = new Stockfish();
+
   // Puts all the components together to create the screen
   public ConsoleGraphics() {
+
+    // this.stockfish = new Stockfish();
+    stockfish.startEngine();
+
     frame = new JFrame("Laboon Chess");
     Container content = frame.getContentPane();   // Get reference to content pane
 
     // Left side of the board has the timer, chess board, and buttons (load and
     // save) stacked vertically
     TimerPanel timer = new TimerPanel();           // Get the timer panel
-    BoardPanel board = new BoardPanel();           // Get the square board
+    BoardPanel board = new BoardPanel(stockfish);           // Get the square board
     ButtonsPanel buttons = new ButtonsPanel();     // Get the buttons panel
 
     JPanel left = new JPanel();                    // Stack the three panels above
@@ -75,6 +80,14 @@ public class ConsoleGraphics {
 		frame.pack();
 		//frame.setSize(screenSize.width,screenSize.height);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    frame.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        stockfish.stopEngine();
+      }
+    });
+
+
     frame.setVisible(true);                     // Do this last
   }
 
@@ -101,12 +114,12 @@ public class ConsoleGraphics {
     ActionListener action = new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         // Placeholder for when we add functionality
-				
+
       }
     };
     return action;
   }
-  
+
   public static void main(String[] args) {
     ConsoleGraphics chessBoard = new ConsoleGraphics();
   }
