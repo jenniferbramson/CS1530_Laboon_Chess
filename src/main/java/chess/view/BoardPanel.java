2c27bca54586e7d9f75cd10f3fcfa01954414e2d
Jason Ly <jal192@pitt.edu>
2016-11-01 18:02:36 -0400

    Made setPieces() method public instead of private

--- src/main/java/chess/view/BoardPanel.java
+++ src/main/java/chess/view/BoardPanel.java
@@ -110,7 +110,7 @@ public class BoardPanel extends JPanel {
   }
 
 	/*-----------------------------------------------------------------------------------*/
-	private void setPieces(){
+	public void setPieces(){
 		//Added to try and draw letters
 		for(int i=0; i<8;i++){
 			for(int j=0; j<8;j++){
