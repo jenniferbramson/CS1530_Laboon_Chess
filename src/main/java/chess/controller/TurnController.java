a9df8ba0edbae54214f9cc17bcec409256a84b62
Jason Ly <jal192@pitt.edu>
2016-11-08 23:54:10 -0500

    Added 50 move rule test for computer player

--- src/main/java/chess/controller/TurnController.java
+++ src/main/java/chess/controller/TurnController.java
@@ -11,6 +11,8 @@ public class TurnController {
   private ConsoleGraphics graphics;
   private boolean graphicsExist = false;
   protected static String resultsOfGame = "noResult";
+  
+  int moveRule50Counter = 0;
 
   public TurnController(char playersColor) {
     turn = 'w';
@@ -84,6 +86,10 @@ public class TurnController {
 	  String move = BoardPanel.playersMostRecentMove;
 	  String fen = BoardPanel.playersFenAfterMove;
 	  
+	  //Update the 50 move rule counter by the one in the previous fen
+	  String[] fenSections = fen.split(" ");
+	  moveRule50Counter = Integer.parseInt(fenSections[4]);
+	  
 	  //Keep track of the last 6 moves
 	  if(BoardPanel.previousMoves.size() < 6) {
 		BoardPanel.previousMoves.add(move);
@@ -94,7 +100,7 @@ public class TurnController {
 	  }
 	  
 	  //Display when testing win/loss/draw condition starts
-	  System.out.println("Starting tests for game results");
+	  System.out.println("Starting tests for game results in changeTurn");
 	  resultsOfGame = BoardPanel.my_rulebook.testGameEnded(fen);
 	  if(!resultsOfGame.equals("noResult")) {
 		GameResults result = new GameResults();
@@ -117,6 +123,10 @@ public class TurnController {
   }
 
    public void playMoveFromStockfish(){
+	String fenBeforeMove = BoardPanel.my_storage.getFen();   
+	int countPiecesBefore = 0;
+	int countPiecesAfter = 0;
+	
     String bestMove = LaboonChess.stockfish.getBestMove(BoardPanel.my_storage.getFen(), 1000);
     System.out.println("best mvoe from stockfish " + bestMove);
     LaboonChess.stockfish.movePiece(bestMove, BoardPanel.my_storage.getFen());
@@ -134,7 +144,58 @@ public class TurnController {
     BoardPanel.my_storage.movePiece(old_y, old_x, y, x);
     LaboonChess.stockfish.drawBoard();
     LaboonChess.changeTurn();
+
+	System.out.println("Move Counter: " + moveRule50Counter);
 	
+	//Check if the move counter before stockfish makes it's move was 49
+	//Indicates that one more move could lead to a draw
+	//Stockfish's next move could initiate a draw
+	if(moveRule50Counter == 49) {
+		//Check if a piece was taken
+		//Need to do this check since stockfish automatically resets the 50 move counter to 0 when it hits 50
+		//Therefore we need to test it manually
+		String[] fenSectionsBeforeMove = fen.split(" ");
+		String fenBefore = fenSectionsBeforeMove[0];
+		char temp;
+		
+		//Count number of pieces before stockfish makes a move
+		for(int i = 0; i < fenBefore.length(); i++) {
+			temp = fenBefore.charAt(i);
+			if(Character.isLetter(temp)) {
+				countPiecesBefore++;
+			}
+		}
+		
+		String fenAfterMove = BoardPanel.my_storage.getFen();
+		String[] fenSectionsAfterMove = fen.split(" ");
+		String fenAfter = fenSectionsAfterMove[0];
+		
+		//Count number of pieces after stockfish makes a move
+		for(int i = 0; i < fenAfter.length(); i++) {
+			temp = fenAfter.charAt(i);
+			if(Character.isLetter(temp)) {
+				countPiecesAfter++;
+			}
+		}
+		
+		System.out.println("Number of pieces before the move: " + countPiecesBefore);
+		System.out.println("Number of pieces after the move: " + countPiecesAfter);
+		
+		//Indicates that stockfish made a move and didn't take piece, therefore it's a draw
+		if(countPiecesBefore == countPiecesAfter) {
+			
+			resultsOfGame = "draw";
+			System.out.println("Draw by 50 move rule");
+			
+			//Update board with move made by stockfish
+			//This implementation avoids "non-static method cannot be referenced from static context error"
+			BoardPanel tempBoardPanel = ConsoleGraphics.board;
+			tempBoardPanel.setPieces();
+			ConsoleGraphics.board = tempBoardPanel;
+			
+			GameResults result = new GameResults();
+		}
+	}
 	//Update board with move made by stockfish
 	//This implementation avoids "non-static method cannot be referenced from static context error"
 	BoardPanel tempBoardPanel = ConsoleGraphics.board;
@@ -150,9 +211,11 @@ public class TurnController {
 		BoardPanel.previousMoves.add(bestMove);
 		BoardPanel.previousMoves.removeFirst();
 	}
+	
+	
 
 	//Display when testing win/loss/draw condition starts
-	System.out.println("Starting tests for game results");
+	System.out.println("Starting tests for game results in playMoveFromStockfish");
 	resultsOfGame = BoardPanel.my_rulebook.testGameEnded(fen);
 	if(!resultsOfGame.equals("noResult")) {
 		GameResults result = new GameResults();
