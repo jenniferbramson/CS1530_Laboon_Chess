003e02dab69bd46c2010598af63009bfca4a6fa0
Jennifer Bramson <jib41@pitt.edu>
2016-11-16 00:43:47 -0500

    Made video local for instant play

--- src/main/java/chess/view/YouTubeVideoPanel.java
+++ src/main/java/chess/view/YouTubeVideoPanel.java
@@ -7,35 +7,46 @@ import javax.imageio.*;
 
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.web.WebView;
-import javafx.scene.web.WebEngine;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.layout.BorderPane;
 
 public class YouTubeVideoPanel extends JPanel {
 
-	protected static WebView webview;
+	protected static MediaPlayer player;
 
 	public YouTubeVideoPanel() {
 		this.setLayout(new BorderLayout());
-		
+
 		//Adding the video to the dialog box
+		Platform.setImplicitExit(false);
 		JFXPanel web = new JFXPanel();
-		web.setPreferredSize(new Dimension(800, 450));
+
+		String url = this.getClass().getResource("/rickroll.mp4").toExternalForm();
+		player = new MediaPlayer(new Media(url));
+		player.setAutoPlay(true);
+
+		// create mediaView and add media player to the viewer
+		MediaView mediaView = new MediaView();
+		mediaView.setFitHeight(450);	//Sets video height
+		mediaView.setMediaPlayer(player);
+		BorderPane pane = new BorderPane();
+    pane.setCenter(mediaView);
+		Scene scene = new Scene(pane, Color.BLACK);
+		web.setScene(scene);
+
 		this.add(web, BorderLayout.CENTER);
-		Platform.runLater(() -> {
-			webview = new WebView();
-			web.setScene(new Scene(webview));
-			webview.getEngine().load("http://www.dailymotion.com/embed/video/xsdji?autoPlay=1");
-		});	
 	}
-	
+
 	public void exit() {
-		Platform.runLater(() -> {
-			webview.getEngine().getLoadWorker().cancel();
-		});	
+		if(player != null) {
+			player.stop();
+			player.dispose();
+		}
 	}
 }
